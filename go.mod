9e73a4525584381d2bd0f098ede549a92502639f
wangzn <wangzn04@gmail.com>
2019-01-15 11:13:36 +0800

    add all

---
+++ go.mod
@@ -0,0 +1,22 @@
+module github.com/wangzn/worm
+
+require (
+	cloud.google.com/go v0.34.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/denisenkom/go-mssqldb v0.0.0-20190111225525-2fea367d496d // indirect
+	github.com/erikstmartin/go-testdb v0.0.0-20160219214506-8d10e4a1bae5 // indirect
+	github.com/facebookgo/ensure v0.0.0-20160127193407-b4ab57deab51 // indirect
+	github.com/facebookgo/errgroup v0.0.0-20160209021148-779c8d7ef069
+	github.com/facebookgo/stack v0.0.0-20160209184415-751773369052 // indirect
+	github.com/facebookgo/subset v0.0.0-20150612182917-8dac2c3c4870 // indirect
+	github.com/go-sql-driver/mysql v1.4.1 // indirect
+	github.com/gofrs/uuid v3.2.0+incompatible // indirect
+	github.com/google/go-cmp v0.2.0 // indirect
+	github.com/jinzhu/gorm v1.9.2
+	github.com/jinzhu/inflection v0.0.0-20180308033659-04140366298a // indirect
+	github.com/jinzhu/now v0.0.0-20181116074157-8ec929ed50c3 // indirect
+	github.com/lib/pq v1.0.0 // indirect
+	github.com/mattn/go-sqlite3 v1.10.0 // indirect
+	golang.org/x/crypto v0.0.0-20190103213133-ff983b9c42bc // indirect
+	google.golang.org/appengine v1.4.0 // indirect
+)
---
+++ worm.go
@@ -0,0 +1,97 @@
+// Copyright 2019 @ wangzn04@gmail.com, Inc. All rights reserved.
+
+// @Author: wangzn
+// @Date: 2019/1/15 11:11 AM
+
+package worm
+
+import (
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/facebookgo/errgroup"
+	"github.com/jinzhu/gorm"
+)
+
+// DB 寸一个db变量
+type DB struct {
+	client *gorm.DB
+	addr   string
+	ts     time.Time
+	mds    []interface{}
+}
+
+// ORM 存到mysql数据库
+type ORM struct {
+	conn    map[string]*DB
+	current *DB
+	sync.RWMutex
+}
+
+// NewORM 返回一个orm
+func NewORM() *ORM {
+	return &ORM{
+		conn: make(map[string]*DB),
+	}
+}
+
+// DB 返回当前的db
+func (m *ORM) DB() *gorm.DB {
+	return m.current.client
+}
+
+// AddDB 加
+func (m *ORM) AddDB(name, addr string) error {
+	m.Lock()
+	defer m.Unlock()
+	db, err := gorm.Open("mysql", addr)
+	if err != nil {
+		return err
+	}
+	if _, ok := m.conn[name]; ok {
+		return fmt.Errorf("db name %s exist", name)
+	}
+	n := &DB{
+		client: db,
+		addr:   addr,
+		ts:     time.Now(),
+		mds:    make([]interface{}, 0),
+	}
+	m.conn[name] = n
+	m.current = n
+	return nil
+}
+
+// SelectDB 选
+func (m *ORM) SelectDB(name string) error {
+	m.Lock()
+	defer m.Unlock()
+	if db, ok := m.conn[name]; ok {
+		m.current = db
+		return nil
+	}
+	return fmt.Errorf("db name %s does not exist", name)
+}
+
+// RegisterModel 把某个model注册到某个db上
+func (m *ORM) RegisterModel(name string, model interface{}) error {
+	m.Lock()
+	defer m.Unlock()
+	if db, ok := m.conn[name]; ok {
+		db.mds = append(db.mds, model)
+		return nil
+	}
+	return fmt.Errorf("db name %s does not exist", name)
+}
+
+// Migration 同步表结构
+func (m *ORM) Migration(name string) error {
+	m.Lock()
+	defer m.Unlock()
+	if db, ok := m.conn[name]; ok {
+		db.client.AutoMigrate(db.mds...)
+		return errgroup.NewMultiError(db.client.GetErrors()...)
+	}
+	return nil
+}
